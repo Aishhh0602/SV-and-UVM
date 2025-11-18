5289e2ac39643bb78ccda147526b0f1a568184df
Aishwarya K <aishwarya110602@gmail.com>
2025-11-18 10:43:25 +0530

    Delete DFF-UVM directory

--- DFF-UVM/interface.sv
+++
@@ -1,5 +0,0 @@
-interface dff_intf (input logic clk);
-  logic rst;
-  logic d;
-  logic q;
-endinterface
--- DFF-UVM/sequencer.sv
+++
@@ -1,8 +0,0 @@
-class dff_sequencer extends uvm_sequencer #(dff_sequence_item);
-  `uvm_component_utils (dff_sequencer)
-  
-  function new (string name ="dff_sequencer",uvm_component parent);
-    super.new(name,parent);
-    `uvm_info("SEQUENCER class", "constructor", UVM_MEDIUM)
-  endfunction
-endclass
